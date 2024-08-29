ff2c3fd382c1859d3220046099ac8e33c188253d
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-08-29 20:17:47 +0200

    EnviromentVariablesTrial

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,37 @@
-'use strict';
+const { google } = require("googleapis");
 
-module.exports.hello = async (event) => {
+// Scopes required to access public calendar events with read-only permission
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
+
+// Extracting configuration variables
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const redirect_uris = ["https://crrenatacr.github.io/meet/"];
+
+// Creating the OAuth2 client with credentials and redirect URI
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+// Function that generates the authentication URL for Google OAuth2
+module.exports.getAuthURL = async () => {
+  // Generating the authentication URL with the required scope
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  // Returning the authentication URL in JSON format
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
