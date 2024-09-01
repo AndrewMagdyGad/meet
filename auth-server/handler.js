c43483fa4ed5563e768bbfb46bfe02399ae33ada
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-09-01 21:43:45 +0200

    FunctionsAndHtmlUpdate

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,7 +1,9 @@
 const { google } = require("googleapis");
 
 // Scopes required to access public calendar events with read-only permission
-const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar.events.public.readonly",
+];
 
 // Extracting configuration variables
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
@@ -26,11 +28,49 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
     },
     body: JSON.stringify({
       authUrl,
     }),
   };
+};
+
+//Function that generates the Access Token
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
 };
