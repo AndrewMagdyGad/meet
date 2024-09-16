cdd2d8c1fca9cc85fd7a75cfd5e236085e78cdf0
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-09-16 21:31:57 +0200

    EventListCitySearchTesting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-// Import the EventList component from the components directory
+import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
-// Import the main CSS file for styling the App component
 import './App.css';
 
-// Define the App component using an arrow function
 const App = () => {
-  // Return the JSX structure, with the EventList component inside a div with class "App"
-  return (
-    <div className="App">
-      {/* Render the EventList component */}
-      <EventList />
-    </div>
-  );
+ return (
+   <div className="App">
+     <CitySearch />
+     <EventList />
+   </div>
+ );
 }
 
-// Export the App component so it can be imported and used in other parts of the application
 export default App;
