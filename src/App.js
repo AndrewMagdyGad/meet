0fb6eefe56af1825cc8081c5b5269eaa3e5c3f4a
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-09-16 17:32:55 +0200

    FirstTestFailingThenPassing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+// Import the main CSS file for the App component
 import './App.css';
 
-function App() {
+// Define the App component as a functional component using an arrow function
+const App = () => {
+  // Return the JSX that describes the UI structure
   return (
     <div className="App">
+      {/* This div will contain the list of events */}
+      <div id="event-list"></div>
     </div>
   );
-}
+};
 
+// Export the App component to make it available for import in other files
 export default App;
