d7ad655a32233ff19eed5884672f0447112c7c4c
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-09-24 14:09:57 +0200

    RenderingNumberOfEvents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents'; // Importando o componente
 import './App.css';
 
 // Main application component
 const App = () => {
   return (
     <div className="App">
+      {/* Render the NumberOfEvents component */}
+      <NumberOfEvents /> {/* Adicionando o componente aqui */}
       {/* Render the CitySearch component */}
       <CitySearch />
       {/* Render the EventList component */}
