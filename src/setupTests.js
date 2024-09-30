4ba2ef07628849a56f71c49ab758d9914b08f6e9
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-09-30 11:45:10 +0200

    GeneralUpdate

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,7 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 
 import '@testing-library/jest-dom';
-import { act } from 'react'; // Importing act from React
+
 
 // Adding portions of the warning messages to intentionally prevent from appearing
 const MESSAGES_TO_IGNORE = [
